0f625c8f2c0f14d0f78e4db1f461b8c05c3dae72
Marco Zampetti <m.zampetti@cantierecreativo.net>
2017-06-15 17:14:36 +0200

    Add autoprefixer in webpack config

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,5 +1,6 @@
-var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
 
 var extractSass = new ExtractTextPlugin('stylesheets/[name].css');
 
@@ -9,7 +10,7 @@ module.exports = {
     styles: __dirname + '/source/stylesheets/application.sass'
   },
   resolve: {
-    root: __dirname + '/source/javascripts',
+    root: __dirname + '/source/javascripts'
   },
   output: {
     path: __dirname + '/.tmp/dist',
@@ -19,12 +20,14 @@ module.exports = {
     loaders: [
       {
         test: /.*\.sass$/,
-        loader: extractSass.extract(['css', 'sass', 'import-glob-loader'])
+        loader: extractSass.extract(['css', 'postcss', 'sass', 'import-glob-loader'])
       }
     ]
   },
   plugins: [
     extractSass
-  ]
+  ],
+  postcss: function postcss() {
+    return [autoprefixer];
+  },
 };
-
