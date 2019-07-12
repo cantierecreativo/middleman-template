148a4f0586e7505aae2b63fbd07d1c9ea94e9b90
damjack <giacomello.damiano@gmail.com>
2019-07-12 19:02:07 +0200

    Fix image_lazy helper

--- optional/config.rb
+++ optional/config.rb
@@ -90,7 +90,7 @@ helpers do
   end
   alias_method :i, :icon
 
-  def image_lazy(image, url_options, sizes = {}, attributes = {})
+  def image_lazy(image, url_options, sizes = [], attributes = {})
     options = attributes.symbolize_keys
     url_options.merge!({auto: 'format,compress'})
 
@@ -98,16 +98,16 @@ helpers do
     options[:alt] ||= image.alt
 
     sizes_string = sizes.map do |width, size|
-      "#{url_options.merge!({w: width})} #{size}w"
+      "#{image.url(url_options.merge!({w: width}))} #{size}w"
     end
     options.merge!({
-      data: {src: image.url(url_options), srcset: sizes_string}
+      data: {src: image.url(url_options), srcset: sizes_string.join(", ")}
     })
 
-    image_tag(image, options)
+    image_tag(image.url(url_options.merge!({w: sizes.first.last})), options)
   end
 
-  def image_tag(path, options_hash = ::Middleman::EMPTY_HASH)
+  def image_tag(path, options_hash = {})
     options = options_hash.dup
     if !options.key?(:class)
       options[:class] = "lazyload"
