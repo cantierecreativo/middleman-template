cf4917308316cec23bef85db0ea52b0928301973
damjack <giacomello.damiano@gmail.com>
2019-07-12 18:29:03 +0200

    Add lazyload

--- template/source/javascripts/index.js
+++ template/source/javascripts/index.js
@@ -6,9 +6,11 @@ import modernizr from 'modernizr';
 import slick from 'slick-carousel';
 import anime from 'animejs/lib/anime';
 import AOS from 'aos';
+import LazyLoad from "lazyload"
 
 $(document).ready(function() {
   AOS.init({disable: 'mobile'});
+  lazyload();
   // CANVAS ASIDE LEFT
   $(".js-nav-toggler--left").click(function(e) {
     e.preventDefault;
